84b03716f4dda63c311d4639034527c077c9c1c9
AbrarShaikh <34272376+AbrarShaikh@users.noreply.github.com>
2024-01-09 00:07:06 +0530

    Add files via upload

---
+++ rvisa_count.py
@@ -0,0 +1,53 @@
+import sys, getopt
+
+def main(argv):
+    inputfile = ''
+    outputfile = ''
+    count = 0
+    
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+        
+    except:
+        print("Unhandled option\n")
+        print("try for help: rvisa_count.py -h ")
+        sys.exit()
+        
+    for opt, arg in opts:
+        if opt == '-h':
+            print(f'{sys.argv[0]} -i <inputfile> -o <outputfile>' + "\n" + "-h this help")
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+            
+    with open(inputfile, 'r') as file:
+        assembly_code = file.read()
+    lines = assembly_code.split('\n')
+    unique_instructions = set()
+    for line in lines[5:]:
+        line = line.strip()
+        if line and not line.startswith('#'):
+            parts = line.split()
+            if len(parts) >= 3:
+                instruction = parts[2]
+                unique_instructions.add(instruction)
+                count += 1
+
+    if outputfile != "":
+        with open(outputfile, 'w') as file:
+            file.write(f"Total Number of instructions: {count}" + "\n")
+            file.write(f"Number of different instructions: {len(unique_instructions)}" + "\n")
+            for instruction in unique_instructions:
+                file.write(instruction + "\n")
+    else:
+        print("Total Number of different instructions: {count}" + "\n")
+        print(f"Number of different instructions: {len(unique_instructions)}")
+        print("List of unique instructions:")
+        for instruction in unique_instructions:
+            print(instruction)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+
